6949dd68fc7281631c1730a9ce0f79e1d49deda2
jonahcui <jonahcui0@gmail.com>
2022-05-06 00:32:28 +0800

    add test

--- test/mutualTokenFactory.js
+++ test/mutualTokenFactory.js
@@ -2,11 +2,26 @@ var MutualTokenFactory = artifacts.require("./MutualTokenFactory.sol");
 
 contract("MutualTokenFactory", accounts => {
 
-    it("should create proposal", async () => {
+    it("should create proposal by special owner", async () => {
+        const mutualTokenFactory = await MutualTokenFactory.deployed({
+            from: accounts[0]
+        });
+
+        const owner = await mutualTokenFactory.owner.call();
+
+        assert.equal(accounts[0], owner);
+    })
+
+
+    it("should create user proposal when join given a not joined user", async () => {
         const mutualTokenFactory = await MutualTokenFactory.deployed({
             from: accounts[0]
         });
 
         const result = await mutualTokenFactory.join(1000, "test", {from: accounts[1]});
-    })
+        const userProposal = await mutualTokenFactory.userJoinedProposal.call(accounts[1]);
+
+        assert.isNotNull(result);
+        assert.equal(true, userProposal.exists)
+    }) 
 })
